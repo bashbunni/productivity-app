6a54b4358523d6a33a4f688f5c46068b887f98ef
Gabrielle Lamarche <lamarchegaby@gmail.com>
2021-01-11 16:10:34 -0800

    moved app to React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
+
+// library.add(fab, fas, far);
+
+
+function Nav() {
+  return (
+    < nav className="sidebar" >
+      <button className="nav-btn" id="home"><FontAwesomeIcon icon={["fas", "igloo"]} />
+        <p className="nav-text">Home</p>
+      </button>
+      <button className="nav-btn" id="slack"><FontAwesomeIcon icon={["fab", "slack-hash"]} />
+        <p className="nav-text">Slack</p>
+      </button>
+      <button className="nav-btn" id="gmail"><FontAwesomeIcon icon={["far", "envelope"]} />
+        <p className="nav-text">Gmail</p>
+      </button>
+      <button className="nav-btn" id="github"><FontAwesomeIcon icon={["fab", "github"]} />
+        <p className="nav-text">Github</p>
+      </button>
+      <button className="nav-btn" id="add"><FontAwesomeIcon icon={["fas", "plus-circle"]} />
+        <p className="nav-text">New</p>
+      </button>
+    </nav >
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
---
+++ src/fontawesome.js
@@ -0,0 +1,9 @@
+/**
+ * Created this file so that the icons can be used throughout the app without needing to be redefined
+ */
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fab, fas, far)
