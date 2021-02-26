61dd22005224f52ea37f63da9ca4d0861ee3c755
bashbunni <bashbunni.io@gmail.com>
2021-02-26 09:48:42 -0800

    literally did nothing

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,35 +5,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Nav = (props) => {
 
     let habits = props.habitList.map((habit, index) => {
-        
-    })
 
-    render() {
-        return (
-            <nav className="sidebar" >
-                <Link to="/">
-                    <button className="nav-btn" id="home"><FontAwesomeIcon icon={["fas", "igloo"]} />
-                        <p className="nav-text">Home</p>
-                    </button>
-                </Link>
-                <Link to="/habit1">
-                    <button className="nav-btn" id="slack"><FontAwesomeIcon icon={["fab", "slack-hash"]} />
-                        <p className="nav-text">Habit1</p>
-                    </button>
-                </Link>
-                <Link to="/habit1">
-                    <button className="nav-btn" id="gmail"><FontAwesomeIcon icon={["far", "envelope"]} />
-                        <p className="nav-text">Habit2</p>
-                    </button>
-                </Link>
-                <Link to="/newhabit">
-                    <button className="nav-btn" id="add" onClick={this.props.toggleModal}><FontAwesomeIcon icon={["fas", "plus-circle"]} />
-                        <p className="nav-text">New</p>
-                    </button>
-                </Link>
-            </nav>
-        );
-    }
+    })
+    return (
+        <nav className="sidebar" >
+            <Link to="/">
+                <button className="nav-btn" id="home"><FontAwesomeIcon icon={["fas", "igloo"]} />
+                    <p className="nav-text">Home</p>
+                </button>
+            </Link>
+            <Link to="/habit1">
+                <button className="nav-btn" id="slack"><FontAwesomeIcon icon={["fab", "slack-hash"]} />
+                    <p className="nav-text">Habit1</p>
+                </button>
+            </Link>
+            <Link to="/habit1">
+                <button className="nav-btn" id="gmail"><FontAwesomeIcon icon={["far", "envelope"]} />
+                    <p className="nav-text">Habit2</p>
+                </button>
+            </Link>
+            <Link to="/newhabit">
+                <button className="nav-btn" id="add" onClick={this.props.toggleModal}><FontAwesomeIcon icon={["fas", "plus-circle"]} />
+                    <p className="nav-text">New</p>
+                </button>
+            </Link>
+        </nav>
+    );
 }
 
 export default Nav; 
--- src/Habit.js
+++ src/Habit.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import create from 'zustand';
+
 
 const Habit = (props) => {
 
